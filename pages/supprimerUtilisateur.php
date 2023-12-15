88288a8faa7a5facbca71d4b51a44d469a2cf15a
Nguyen Constant <constant.nguyen@iut-rodez.fr>
2023-12-15 11:39:19 +0100

    Ajout des pages supprimer et modifer utilisateur

--- pages/supprimerUtilisateur.php
+++ pages/supprimerUtilisateur.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salon Eureka</title>
+    <title>Supprimer un utilisateur</title>
 
     <!-- css -->
     <link rel="stylesheet" href="../css/main.css">
@@ -65,7 +65,7 @@
 
     <div class="container">
         <div class="container-content">
-            <span class="titre-panel-ouvert"><span>Gestion des entreprises ></span> Ajouter une entreprise</span>
+            <span class="titre-panel-ouvert"><span>ICI CA SUPPRIME OU QUOI</span> SUPPRIMER UN UTILISATEUR ICI</span>
         </div>
 
         <div class="container-asyde">
@@ -104,8 +104,8 @@
                         <nav>
                             <ul>
                                 <li class="hover-underline-static">Ajouter un utilisateur</li>
-                                <li class="hover-underline-static">Modifier un utilisateur</a></li>
-                                <li class="hover-underline-static">Supprimer un utilisateur</a></li>
+                                <li class="hover-underline-static"><a href="./modifierUtilisateur.php">Modifier un utilisateur</a></li>
+                                <li class="hover-underline-static"><a href="./supprimerUtilisateur.php">Supprimer un utilisateur</a></li>
                                 <li class="hover-underline-static">Retrouver un mot de passe</li>
                             </ul>
                         </nav>
